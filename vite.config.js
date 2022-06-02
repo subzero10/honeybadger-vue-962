5432ba6ef041e271b2616c7b3c896baa26d286a1
Jacob Knobel <jk@datapult.dk>
2022-06-02 11:05:59 +0200

    more realistic vite config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,17 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import {defineConfig} from "vite";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig(({command}) => ({
+  base: command === "serve" ? process.env.ASSET_URL || "" : `${process.env.ASSET_URL || ""}/build/`,
+  mode: "production",
+  publicDir: false,
+  build: {
+    sourcemap: true,
+    manifest: true,
+    outDir: "public/build",
+    rollupOptions: {
+      input: "src/main.js",
+    },
+  },
   plugins: [vue()],
 }));

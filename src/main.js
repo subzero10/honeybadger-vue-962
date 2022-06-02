bb5064701a4ac76a8b6a8e27d238ccb4aad97918
Jacob Knobel <jk@datapult.dk>
2022-06-02 11:07:47 +0200

    tried nesting the use-statement in a conditional (like is the case in my app) to see if the causes issues

--- src/main.js
+++ src/main.js
@@ -1,11 +1,19 @@
 import { createApp } from 'vue'
+import {createPinia} from 'pinia'
+import {createRouter, createWebHistory} from 'vue-router';
 import App from './App.vue'
 import HoneybadgerVue from '@honeybadger-io/vue'
 
 const app = createApp(App)
-app.use(HoneybadgerVue, {
-  apiKey: '80ee8156',
-  environment: 'vue-962',
-})
+
+if (import.meta.env.MODE === "production") {
+  app.use(HoneybadgerVue, {
+    apiKey: '80ee8156',
+  })
+  console.log("Using Honeybadger")
+}
+
+app.use(createPinia())
+app.use(createRouter({history: createWebHistory(), routes: []}))
 
 app.mount('#app')
